092a23e8ef65eb3b67e9d2572739ae8ef567a858
Divyash Patel <divyashpa@hotmail.com>
2018-01-07 20:46:27 -0600

    add

---
+++ RelianseGlobal.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RelianseGlobal.Web.MvcApplication" Language="C#" %>
---
+++ RelianseGlobal.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RelianseGlobal.Web.Startup))]
+
+namespace RelianseGlobal.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
